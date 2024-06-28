04c34aa821e0aa81802d8aafece40b21b9e7b2f3
egjallow <egjallow7@gmail.com>
2024-06-28 23:06:37 +0000

    create file in tep

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# create a file an ensure it have content
-
+# create a file in `/tmp`.
 file {'/tmp/school':
-    ensure => 'present',
-    content => 'I love Puppet',
-    group => 'www-data',
-    mode => '0744',
-    owner => 'www-data',
-    path => '/tmp/school'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
