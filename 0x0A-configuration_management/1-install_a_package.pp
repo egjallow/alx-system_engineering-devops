7afd7b644526ec3483c257ae50385b9e366bdda1
egjallow <egjallow7@gmail.com>
2024-06-28 23:11:25 +0000

    kill a process and install pakage

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#Using Puppet, create a manifest that kills a process named `killmenow`.
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
+# installs puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
