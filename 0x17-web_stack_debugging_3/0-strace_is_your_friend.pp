671cccc38324039d258508be7decd17b9a328d8e
egjallow <egjallow7@gmail.com>
2024-08-17 20:06:51 -0400

    puppet file modify  to fix wordpress issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,7 +3,5 @@
 exec { 'fix_wordpress_php_typo':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/bin'],
-  onlyif  => 'grep phpp /var/www/html/wp-settings.php',
-  require => Service['apache2'],
 }
 
